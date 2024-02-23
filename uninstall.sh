34a3fac3aabf7fc8dd42beee4096392f9ba4df59
tomxi1997 <78306449+tomxi1997@users.noreply.github.com>
2024-02-23 10:22:17 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -12,4 +12,4 @@ sleep 1
 mv /system/bin/bootlinux /system/bin/bootlinux.bak
 
 #请确保所有挂载都已取消，否则十分危险，有变砖，数据丢失风险。建议是运行手动运行killlinux /data/ubuntuxx，lhroot选择移除chroot 。然后在执行以下命令，或者直接别卸载吧。。
-#rm -rf /data/ubuntu*
+#rm -rf /data/ubuntu22 /data/ubuntu20 /data/ubuntu18
